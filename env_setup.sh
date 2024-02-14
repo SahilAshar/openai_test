e28713b4831a8a79529c25c15cda98d0a538c1a2
Sahil Ashar <sahilashar16@gmail.com>
2024-02-13 19:56:22 -0800

    got the quickstart working

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+# https://stackoverflow.com/questions/6590688/is-it-bad-to-have-my-virtualenv-directory-inside-my-git-repository
+
+# after deprecation of some arguments in 15.1.0
+virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
